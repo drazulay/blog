f5367281540ce8a8f6aebd52ee24d0b3e9bc732b
Daniel R. Azulay <d.r.azulay@gmail.com>
2021-03-22 22:46:34 +0100

    Fetch method

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,6 @@ var urlsToCache = [
   '/',
   '/logo96.png',
   '/logo144.png',
-  '/styles.css',
 ];
 
 self.addEventListener('install', function(event) {
@@ -13,4 +12,30 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME).then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+            return response;
+          }
+        );
+      })
+    );
 });
