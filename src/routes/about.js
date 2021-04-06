41cc76f7477ca0c6baa0cf3dc35ffabb3f00d607
Daniel Azulay <daniel@danielazulay.eu>
2021-04-06 13:38:06 +0200

    Buy me a coffee :)

--- src/routes/about.js
+++ src/routes/about.js
@@ -23,6 +23,11 @@ class About extends React.Component {
             &#128273;{" "}<a href="https://keybase.io/drazulay">5D54 61A3 9955 2A8A</a>
           </span>
         </p>
+        <p>
+          <span className="PostTitle">
+            &#9749;{" "}<a href="https://bmac.vecha.in/donate?name=Daniel%20Azulay&addr=0x93dd1812b51b2AB46bbbbD8DD89e29cb29Ab8D84&amount=10&msg=It's%20a%20cliche%20but%20I%20turn%20caffeine%20into%20code.">Buy me a coffee :)</a>
+          </span>
+        </p>
     </div>;
   }
 }
